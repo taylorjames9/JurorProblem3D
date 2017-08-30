b118da7d14cef0bca91e7160222b423d630e74d9
taylorjames9 <taylorjames9@gmail.com>
2017-08-29 22:22:58 -0700

    carrying an object at an offset now works

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HUX;
 
 public class Item : MonoBehaviour {
 
     public bool Grabbed { get { return grabbed; } set { grabbed = value; } }
 	public Character Owner{ get { return owner; } set { owner = value; } }
     public bool Dislodged { get { return dislodged; } set { dislodged = value; } }
+    public Vector3 CarryOffset { get { return carryOffset; } set { carryOffset = value; } }
+	public float CarryDistance { get { return carryDistance; } set { carryDistance = value; } }
 
+
+	void Update(){
+        if (grabbed)
+        {
+            carryDistance = Vector3.Distance(Veil.Instance.HeadTransform.position, transform.position);
+            transform.position = Veil.Instance.HeadTransform.position + (Veil.Instance.HeadTransform.forward * carryDistance);
+
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -21,11 +33,6 @@ public class Item : MonoBehaviour {
         Owner.MyQueue.ReDrawQueue();
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        
-    }
-
     private void OnTriggerExit(Collider other)
     {
 		if (other.name.Contains("holdingspot"))
@@ -34,8 +41,6 @@ public class Item : MonoBehaviour {
 		}
     }
 
-
-
     public void SetOwner(Character character){
         owner = character;
     }
@@ -43,4 +48,6 @@ public class Item : MonoBehaviour {
 	private bool grabbed;
 	private Character owner;
 	private bool dislodged;
+    private Vector3 carryOffset;
+    private float carryDistance;
 }
--- Assets/Scripts/MousePointToRay.cs
+++ Assets/Scripts/MousePointToRay.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class MousePointToRay : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0))
+        {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
-				Debug.DrawRay(ray.origin, ray.direction *100, Color.red, 0.1f, true);
-				Debug.Log("name of hit "+hit.transform.name);
+                Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 0.1f, true);
+            if (hit.transform != null)
+            {
+                Debug.Log("name of hit " + hit.transform.name);
+                if (hit.transform.GetComponent<Item>() != null)
+                {
+                    hit.transform.GetComponent<Item>().Grabbed = true;
+                    hit.transform.GetComponent<Item>().CarryOffset = HUX.Veil.Instance.HeadTransform.position - transform.position;
+                    hit.transform.GetComponent<Item>().CarryDistance = Vector3.Distance(HUX.Veil.Instance.HeadTransform.position, transform.position);
+                    Debug.Log("Item name is : " + hit.transform.name + " " + "grabbed state is: " + hit.transform.GetComponent<Item>().Grabbed);
+                    Debug.Log("Carry Offset is set to: " + hit.transform.GetComponent<Item>().CarryOffset);
+                }
+            }
         }
 	}
 }
